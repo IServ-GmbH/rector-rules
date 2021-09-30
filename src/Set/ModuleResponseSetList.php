5ad7a29b2ef27891ef747b01bab1a9c6aecbe8a9
Tim Siebels <tim.siebels@iserv.eu>
2021-09-30 13:21:58 +0200

    Added rector for module-response (fixes #42696)

---
+++ src/Set/ModuleResponseSetList.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IServ\Library\RectorRules\Set;
+
+use Rector\Set\Contract\SetListInterface;
+
+final class ModuleResponseSetList implements SetListInterface
+{
+    public const MODULE_RESPONSE_2 = __DIR__ . '/../../config/sets/iserv/module-response/module-response2.php';
+}
