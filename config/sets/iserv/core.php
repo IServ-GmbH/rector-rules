d0901ffe286851ce8a4fc98c5def8e5eff4e5f47
Matthias Althaus <matthias.althaus@iserv.eu>
2021-10-05 09:28:40 +0200

    Fixed namespaces in IServ sets.

--- config/sets/iserv/core.php
+++ config/sets/iserv/core.php
@@ -19,22 +19,22 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             // Replace deprecated classes
             RenameClassRector::OLD_TO_NEW_CLASSES => [
                 // IServ
-                '\IServ\CoreBundle\Controller\PageController' => '\IServ\CoreBundle\Controller\AbstractPageController',
-                '\IServ\CoreBundle\Entity\GroupRepository' => '\IServ\CoreBundle\Repository\GroupRepository',
-                '\IServ\CoreBundle\Entity\LogRepository' => '\IServ\CoreBundle\Repository\LogRepository',
-                '\IServ\CoreBundle\Entity\MenuItemRepository' => '\IServ\CoreBundle\Repository\MenuItemRepository',
-                '\IServ\CoreBundle\Entity\NotificationRepository' => '\IServ\CoreBundle\Repository\NotificationRepository',
-                '\IServ\CoreBundle\Entity\PrivilegeRepository' => '\IServ\CoreBundle\Repository\PrivilegeRepository',
-                '\IServ\CoreBundle\Entity\UserRepository' => '\IServ\CoreBundle\Repository\UserRepository',
-                '\IServ\CoreBundle\Exception\TypeException' => '\IServ\Library\Common\Exception\TypeException',
-                '\IServ\CoreBundle\Service\ActAdm' => '\IServ\CoreBundle\Backend\ActAdm',
-                '\IServ\CoreBundle\Service\Config' => '\IServ\Library\Config\Config',
-                '\IServ\CoreBundle\Twig\BootstrapSettings' => '\IServ\BootstrapBundle\Twig\BootstrapSettings',
-                '\IServ\CoreBundle\Traits\PrivateConstructorTrait' => '\IServ\Library\Common\Traits\PrivateConstructorTrait',
-                '\Braincrafted\Bundle\BootstrapBundle\Form\Type\BootstrapCollectionType' => '\IServ\BootstrapBundle\Form\Type\BootstrapCollectionType',
-                '\Braincrafted\Bundle\BootstrapBundle\Form\Type\FormActionsType' => '\IServ\BootstrapBundle\Form\Type\FormActionsType',
-                '\Braincrafted\Bundle\BootstrapBundle\Form\Type\FormStaticControlType' => '\IServ\BootstrapBundle\Form\Type\FormStaticControlType',
-                '\Braincrafted\Bundle\BootstrapBundle\Form\Type\MoneyType' => '\IServ\BootstrapBundle\Form\Type\MoneyType',
+                'IServ\CoreBundle\Controller\PageController' => 'IServ\CoreBundle\Controller\AbstractPageController',
+                'IServ\CoreBundle\Entity\GroupRepository' => 'IServ\CoreBundle\Repository\GroupRepository',
+                'IServ\CoreBundle\Entity\LogRepository' => 'IServ\CoreBundle\Repository\LogRepository',
+                'IServ\CoreBundle\Entity\MenuItemRepository' => 'IServ\CoreBundle\Repository\MenuItemRepository',
+                'IServ\CoreBundle\Entity\NotificationRepository' => 'IServ\CoreBundle\Repository\NotificationRepository',
+                'IServ\CoreBundle\Entity\PrivilegeRepository' => 'IServ\CoreBundle\Repository\PrivilegeRepository',
+                'IServ\CoreBundle\Entity\UserRepository' => 'IServ\CoreBundle\Repository\UserRepository',
+                'IServ\CoreBundle\Exception\TypeException' => 'IServ\Library\Common\Exception\TypeException',
+                'IServ\CoreBundle\Service\ActAdm' => 'IServ\CoreBundle\Backend\ActAdm',
+                'IServ\CoreBundle\Service\Config' => 'IServ\Library\Config\Config',
+                'IServ\CoreBundle\Twig\BootstrapSettings' => 'IServ\BootstrapBundle\Twig\BootstrapSettings',
+                'IServ\CoreBundle\Traits\PrivateConstructorTrait' => 'IServ\Library\Common\Traits\PrivateConstructorTrait',
+                'Braincrafted\Bundle\BootstrapBundle\Form\Type\BootstrapCollectionType' => 'IServ\BootstrapBundle\Form\Type\BootstrapCollectionType',
+                'Braincrafted\Bundle\BootstrapBundle\Form\Type\FormActionsType' => 'IServ\BootstrapBundle\Form\Type\FormActionsType',
+                'Braincrafted\Bundle\BootstrapBundle\Form\Type\FormStaticControlType' => 'IServ\BootstrapBundle\Form\Type\FormStaticControlType',
+                'Braincrafted\Bundle\BootstrapBundle\Form\Type\MoneyType' => 'IServ\BootstrapBundle\Form\Type\MoneyType',
             ],
         ]])
     ;
@@ -43,11 +43,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->call('configure', [[
             // Replace deprecated static method calls
             RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => ValueObjectInliner::inline([
-                new RenameStaticMethod('\IServ\CoreBundle\Util\Format', 'linkify', '\IServ\CoreBundle\Util\Linkify', 'format'),
-                new RenameStaticMethod('\IServ\CoreBundle\Util\Date', 'nowImmutable', '\IServ\Library\Zeit\Zeit', 'now'),
-                new RenameStaticMethod('\IServ\CoreBundle\Util\Date', 'nowUtcImmutable', '\IServ\Library\Zeit\Zeit', 'nowUtc'),
-                new RenameStaticMethod('\IServ\CoreBundle\Util\Date', 'createImmutable', '\IServ\Library\Zeit\Zeit', 'create'),
-                new RenameStaticMethod('\IServ\CoreBundle\Util\Date', 'createImmutableFromTimestamp', '\IServ\Library\Zeit\Zeit', 'createFromTimestamp'),
+                new RenameStaticMethod('IServ\CoreBundle\Util\Format', 'linkify', 'IServ\CoreBundle\Util\Linkify', 'format'),
+                new RenameStaticMethod('IServ\CoreBundle\Util\Date', 'nowImmutable', 'IServ\Library\Zeit\Zeit', 'now'),
+                new RenameStaticMethod('IServ\CoreBundle\Util\Date', 'nowUtcImmutable', 'IServ\Library\Zeit\Zeit', 'nowUtc'),
+                new RenameStaticMethod('IServ\CoreBundle\Util\Date', 'createImmutable', 'IServ\Library\Zeit\Zeit', 'create'),
+                new RenameStaticMethod('IServ\CoreBundle\Util\Date', 'createImmutableFromTimestamp', 'IServ\Library\Zeit\Zeit', 'createFromTimestamp'),
             ]),
         ]])
     ;
@@ -56,14 +56,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->call('configure', [[
             // Replace deprecated constants
             RenameClassConstFetchRector::CLASS_CONSTANT_RENAME => ValueObjectInliner::inline([
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\BootstrapSettings', 'FORM_BREAKPOINT', 'FORM_BREAKPOINT', '\IServ\BootstrapBundle\Twig\BootstrapSettings'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\BootstrapSettings', 'LABEL_COL_SIZE', 'LABEL_COL_SIZE', '\IServ\BootstrapBundle\Twig\BootstrapSettings'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\BootstrapSettings', 'WIDGET_COL_SIZE', 'WIDGET_COL_SIZE', '\IServ\BootstrapBundle\Twig\BootstrapSettings'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'DEFAULT_ICON_SIZE', 'DEFAULT_ICON_SIZE', '\IServ\CoreBundle\Service\IconRenderer'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'ICON_FUGUE', 'ICON_FUGUE', '\IServ\CoreBundle\Service\IconRenderer'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'ICON_ISERV', 'ICON_ISERV', '\IServ\CoreBundle\Service\IconRenderer'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'ICON_LEGACY', 'ICON_LEGACY', '\IServ\CoreBundle\Service\IconRenderer'),
-                new RenameClassAndConstFetch('\IServ\CoreBundle\Entity\Role', 'DEFAULT_ROLE', 'ROLE_DEFAULT', '\IServ\CoreBundle\Entity\User'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\BootstrapSettings', 'FORM_BREAKPOINT', 'FORM_BREAKPOINT', 'IServ\BootstrapBundle\Twig\BootstrapSettings'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\BootstrapSettings', 'LABEL_COL_SIZE', 'LABEL_COL_SIZE', 'IServ\BootstrapBundle\Twig\BootstrapSettings'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\BootstrapSettings', 'WIDGET_COL_SIZE', 'WIDGET_COL_SIZE', 'IServ\BootstrapBundle\Twig\BootstrapSettings'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'DEFAULT_ICON_SIZE', 'DEFAULT_ICON_SIZE', 'IServ\CoreBundle\Service\IconRenderer'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'ICON_FUGUE', 'ICON_FUGUE', 'IServ\CoreBundle\Service\IconRenderer'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'ICON_ISERV', 'ICON_ISERV', 'IServ\CoreBundle\Service\IconRenderer'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Twig\Extension\IServCoreExtension', 'ICON_LEGACY', 'ICON_LEGACY', 'IServ\CoreBundle\Service\IconRenderer'),
+                new RenameClassAndConstFetch('IServ\CoreBundle\Entity\Role', 'DEFAULT_ROLE', 'ROLE_DEFAULT', 'IServ\CoreBundle\Entity\User'),
             ]),
         ]])
     ;
